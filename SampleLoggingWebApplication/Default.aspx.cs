4a0ff0281228583768c49f97d3868046625a955c
Corn Mendoza <cmendoza@pivotal.io>
2017-09-11 12:31:43 -0500

    Initial Commit

---
+++ SampleLoggingWebApplication/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleLoggingWebApplication
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("I am here at the default page!");
+
+            Logger logger = LogManager.GetLogger("pcfconsole");
+
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
+
+            // alternatively you can call the Log() method
+            // and pass log level as the parameter.
+            logger.Log(LogLevel.Info, "Sample informational message");
+        }
+    }
+}
